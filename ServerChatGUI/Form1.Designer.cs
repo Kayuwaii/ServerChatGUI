a22c63071e3729db1475b61d84a93bcfcbd144e3
alexcoralt <alexcoralt@1ASIX-PC15->
2017-11-02 12:43:00 +0100

    Works but not weel

--- ServerChatGUI/Form1.Designer.cs
+++ ServerChatGUI/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inMessage_txtbox = new System.Windows.Forms.TextBox();
+            this.send_btn = new System.Windows.Forms.Button();
+            this.connection_lbl = new System.Windows.Forms.Label();
+            this.chatDisplay_txtbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inMessage_txtbox
+            // 
+            this.inMessage_txtbox.Location = new System.Drawing.Point(12, 492);
+            this.inMessage_txtbox.Name = "inMessage_txtbox";
+            this.inMessage_txtbox.Size = new System.Drawing.Size(316, 20);
+            this.inMessage_txtbox.TabIndex = 0;
+            // 
+            // send_btn
+            // 
+            this.send_btn.Location = new System.Drawing.Point(335, 489);
+            this.send_btn.Name = "send_btn";
+            this.send_btn.Size = new System.Drawing.Size(75, 23);
+            this.send_btn.TabIndex = 1;
+            this.send_btn.Text = "SEND";
+            this.send_btn.UseVisualStyleBackColor = true;
+            this.send_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // connection_lbl
+            // 
+            this.connection_lbl.AutoSize = true;
+            this.connection_lbl.Location = new System.Drawing.Point(332, 9);
+            this.connection_lbl.Name = "connection_lbl";
+            this.connection_lbl.Size = new System.Drawing.Size(79, 13);
+            this.connection_lbl.TabIndex = 2;
+            this.connection_lbl.Text = "Not Connected";
+            // 
+            // chatDisplay_txtbox
+            // 
+            this.chatDisplay_txtbox.Location = new System.Drawing.Point(12, 43);
+            this.chatDisplay_txtbox.Multiline = true;
+            this.chatDisplay_txtbox.Name = "chatDisplay_txtbox";
+            this.chatDisplay_txtbox.ReadOnly = true;
+            this.chatDisplay_txtbox.Size = new System.Drawing.Size(398, 440);
+            this.chatDisplay_txtbox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 527);
+            this.Controls.Add(this.chatDisplay_txtbox);
+            this.Controls.Add(this.connection_lbl);
+            this.Controls.Add(this.send_btn);
+            this.Controls.Add(this.inMessage_txtbox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inMessage_txtbox;
+        private System.Windows.Forms.Button send_btn;
+        private System.Windows.Forms.Label connection_lbl;
+        private System.Windows.Forms.TextBox chatDisplay_txtbox;
     }
 }
 
--- ServerChatGUI/Form1.cs
+++ ServerChatGUI/Form1.cs
@@ -1,20 +1,160 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ServerChatGUI
 {
     public partial class Form1 : Form
     {
+        public Timer timer1;
+        public TcpListener myList = null;
+
+        public string EncryptionKey = GetHashedKey("Alexandros");
+
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("172.17.1.241");
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                myList = new TcpListener(ipAd, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+
+                Socket s = myList.AcceptSocket();
+                this.Show();
+
+                chatDisplay_txtbox.AppendText("Connection accepted from " + s.RemoteEndPoint + "\n");
+                connection_lbl.Text = "Connected";
+                connection_lbl.ForeColor = Color.Green;
+
+
+                //InitTimer();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.ToString());
+            }
+
+
+        }
+
+        public static string GetHashedKey(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            int cntr = 0;
+            foreach (byte x in hash)
+            {
+                if (cntr == 1)
+                {
+                    cntr = 0;
+                }
+                else
+                {
+                    hashString += String.Format("{0:x2}", x);
+                    cntr++;
+                }
+            }
+            return hashString;
+        }
+
+        //Encrypting a string
+        public static string TxtEncrypt(string inText, string key)
+        {
+            byte[] bytesBuff = Encoding.UTF8.GetBytes(inText);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes crypto = new Rfc2898DeriveBytes(key, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                aes.Key = crypto.GetBytes(32);
+                aes.IV = crypto.GetBytes(16);
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                        cStream.Close();
+                    }
+                    inText = Convert.ToBase64String(mStream.ToArray());
+                }
+            }
+            return inText;
+        }
+
+        //Decrypting a string
+        public static string TxtDecrypt(string cryptTxt, string key)
+        {
+            cryptTxt = cryptTxt.Replace(" ", "+");
+            cryptTxt = cryptTxt.Replace("\0", "");
+            byte[] bytesBuff = Convert.FromBase64String(cryptTxt);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes crypto = new Rfc2898DeriveBytes(key, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                aes.Key = crypto.GetBytes(32);
+                aes.IV = crypto.GetBytes(16);
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                        cStream.Close();
+                    }
+                    cryptTxt = Encoding.UTF8.GetString(mStream.ToArray());
+                }
+            }
+            return cryptTxt;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Socket s = myList.AcceptSocket();
+            chatDisplay_txtbox.AppendText("Me:\t ");
+            chatDisplay_txtbox.AppendText(inMessage_txtbox.Text + "\n");
+
+
+            String str = TxtEncrypt(inMessage_txtbox.Text, EncryptionKey);
+            s.Send(Encoding.UTF8.GetBytes(str));
+
+        }
+
+        public void InitTimer()
+        {
+            timer1 = new Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 200; // in miliseconds
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Socket s = myList.AcceptSocket();
+            if (s.Available > 0)
+            {
+                byte[] b = new byte[s.ReceiveBufferSize];
+                int k = s.Receive(b);
+                string msg = "";
+                chatDisplay_txtbox.AppendText("Other:\t");
+                for (int i = 0; i < k; i++)
+                {
+                    msg += Convert.ToChar(b[i]);
+                }
+                chatDisplay_txtbox.AppendText(TxtDecrypt(msg, EncryptionKey) + "\n");
+            }
         }
     }
 }
